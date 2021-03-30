a7d200abefd1266252a1d71c85251d7ae6e31344
Niloufar Ghesmati <Niloofargst@gmail.com>
2021-03-30 08:15:43 +0200

    updating

--- ex06/src/Plant.js
+++ ex06/src/Plant.js
@@ -3,7 +3,9 @@
 import React from 'react';
 
 const Plant = () => {
-    return <h2>This is a Plant Component!</h2>;
+    return (
+        <h2>This is a Plant Component!</h2>
+    )
 };
 
 export default Plant;
