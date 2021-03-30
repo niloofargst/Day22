a5c6029a00cb1f7101bf55157c72896b1b500f32
Niloufar Ghesmati <Niloofargst@gmail.com>
2021-03-30 08:33:59 +0200

    updating

--- ex03/src/Plant.js
+++ ex03/src/Plant.js
@@ -4,9 +4,7 @@ import React, { Component } from 'react';
 
 class Plant extends Component {
     render() {
-        return (
-            <h2>This is a Plant Component!</h2>
-        )
+        return <h2>This is a Plant Component!</h2>
     }
 }
 
